5182d2dee9cc7127e84b78c72be300d13a75aef5
Atalay Kutlay <atalaykutlaytr@gmail.com>
2021-11-29 20:30:58 -0500

    Updating code with presentation code

--- R/better_print.R
+++ R/better_print.R
@@ -1,4 +1,18 @@
+#' Better Print
+#'
+#' This function prints the given arguments in the best possible way. Unlike R's base function,
+#' function can take unlimited arguments, and simply puts them together.
+#'
+#' @param ... Arguments to print
+#' @return No return, just prints the output.
+#' @export
 better_print = function(...) {
-    things_to_print = list(...)
-    print(things_to_print)
+  arguments = list(...)
+  types = c()
+  for(argument in arguments) {
+    types = c(types, typeof(argument))
+  }
+  if(all(types == "character")) {
+    print(paste0(arguments))
+  }
 }
